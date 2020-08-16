2278c8f9a44138444305cac03d0202f3b5573041
Michael Fahim <michael_fahim@yahoo.com>
2020-08-16 01:10:15 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++
@@ -1,8 +0,0 @@
-class Song < ActiveRecord::Base
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
-  validates :name, presence: true
-  belongs_to :artist
-  has_many :song_genres
-  has_many :genres, through: :song_genres
-end
